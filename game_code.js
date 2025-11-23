f130ccd458d9f2950babfd18f1e04c83192710d8
Ronit <ronitkbiswas@gmail.com>
2025-11-23 10:39:11 +0530

    changes

--- game_code.js
+++ game_code.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const rand = Math.trunc(Math.random() * 20) + 1;
+const rand = Math.trunc(Math.random() * 10) + 1;
 // document.querySelector(".rn").textContent = rand;
 
 document.querySelector(".btn").addEventListener("click", function () {
@@ -9,6 +9,7 @@ document.querySelector(".btn").addEventListener("click", function () {
     document.querySelector(".message").textContent = "!! Number Empty !!";
   } else if (guess === rand) {
     document.querySelector(".message").textContent = " WOW! You Won!!";
+    document.querySelector(".rn").textContent = rand;
   } else if (guess < rand) {
     document.querySelector(".message").textContent = "Too Low !!!";
   } else if (guess > rand) {
