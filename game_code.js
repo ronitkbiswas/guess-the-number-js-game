d78deca119c63db0ca1e2fd6d228e96ed28f8496
Ronit <ronitkbiswas@gmail.com>
2025-11-23 14:44:04 +0530

    updated the entire game

--- game_code.js
+++ game_code.js
@@ -1,7 +1,9 @@
 "use strict";
 
 const rand = Math.trunc(Math.random() * 10) + 1;
-// document.querySelector(".rn").textContent = rand;
+document.querySelector(".rn").textContent = rand;
+let cl = 10;
+document.querySelector(".chancesLeft").textContent = "Chances left: " + cl;
 
 document.querySelector(".btn").addEventListener("click", function () {
   const guess = Number(document.querySelector(".text").value);
@@ -10,9 +12,23 @@ document.querySelector(".btn").addEventListener("click", function () {
   } else if (guess === rand) {
     document.querySelector(".message").textContent = " WOW! You Won!!";
     document.querySelector(".rn").textContent = rand;
+    document.querySelector(".chancesLeft").textContent =
+      "You had only " + cl + " chances left.. Congratulations!";
   } else if (guess < rand) {
     document.querySelector(".message").textContent = "Too Low !!!";
+    cl--;
+    document.querySelector(".chancesLeft").textContent = "Chances left: " + cl;
+    if (cl <= 0) {
+      document.querySelector(".message").textContent = "GAME OVER!!";
+      document.querySelector(".chancesLeft").textContent = "";
+    }
   } else if (guess > rand) {
     document.querySelector(".message").textContent = "Too High !!!";
+    cl--;
+    document.querySelector(".chancesLeft").textContent = "Chances left: " + cl;
+    if (cl <= 0) {
+      document.querySelector(".message").textContent = "GAME OVER!!";
+      document.querySelector(".chancesLeft").textContent = "";
+    }
   }
 });
