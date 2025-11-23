4fad79c1fbd943791003b694f9fc869eaed477ce
Ronit <ronitkbiswas@gmail.com>
2025-11-23 15:02:54 +0530

    Fix merge conflict in game_code.js

--- game_code.js
+++ game_code.js
@@ -2,21 +2,20 @@
 
 const rand = Math.trunc(Math.random() * 10) + 1;
 document.querySelector(".rn").textContent = rand;
+
 let cl = 10;
 document.querySelector(".chancesLeft").textContent = "Chances left: " + cl;
 
 document.querySelector(".btn").addEventListener("click", function () {
   const guess = Number(document.querySelector(".text").value);
+
   if (!guess) {
     document.querySelector(".message").textContent = "!! Number Empty !!";
   } else if (guess === rand) {
     document.querySelector(".message").textContent = " WOW! You Won!!";
     document.querySelector(".rn").textContent = rand;
-<<<<<<< HEAD
     document.querySelector(".chancesLeft").textContent =
       "You had only " + cl + " chances left.. Congratulations!";
-=======
->>>>>>> 365727b128412187e434cd5c705ac37e4f1448b6
   } else if (guess < rand) {
     document.querySelector(".message").textContent = "Too Low !!!";
     cl--;
