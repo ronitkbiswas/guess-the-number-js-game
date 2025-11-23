5392182f0706aa6bcc8ed9565a970d9c9b145a27
Ronit <ronitkbiswas@gmail.com>
2025-11-23 09:45:51 +0530

    formatted

--- game_code.js
+++ game_code.js
@@ -1,3 +1,17 @@
 "use strict";
 
-// nothing here now
+const rand = Math.trunc(Math.random() * 20) + 1;
+// document.querySelector(".rn").textContent = rand;
+
+document.querySelector(".btn").addEventListener("click", function () {
+  const guess = Number(document.querySelector(".text").value);
+  if (!guess) {
+    document.querySelector(".message").textContent = "!! Number Empty !!";
+  } else if (guess === rand) {
+    document.querySelector(".message").textContent = " WOW! You Won!!";
+  } else if (guess < rand) {
+    document.querySelector(".message").textContent = "Too Low !!!";
+  } else if (guess > rand) {
+    document.querySelector(".message").textContent = "Too High !!!";
+  }
+});
