b99cf746a7d1008b94c8f4626658f194b3509e5c
Ronit <ronitkbiswas@gmail.com>
2025-12-03 16:48:12 +0530

    implemented HighScore function & little css

--- game_code.js
+++ game_code.js
@@ -3,7 +3,7 @@
 let sn = Math.trunc(Math.random() * 10) + 1;
 //document.querySelector(".number").textContent = sn;
 let score = 10;
-
+let highscore = 0;
 let displayResult = function (message) {
   document.querySelector(".message").textContent = message;
 };
@@ -15,10 +15,15 @@ document.querySelector(".btn").addEventListener("click", function () {
   } else if (guess === sn) {
     displayResult("🎉 Congratulations !");
     document.querySelector(".number").textContent = sn;
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector(".highscore").textContent =
+        "🏆 High Score: " + highscore;
+    }
   } else if (guess !== sn) {
     displayResult(guess < sn ? "👇 Too Low !" : "👆 Too High !");
     score--;
-    document.querySelector(".score").textContent = "Score: " + score;
+    document.querySelector(".score").textContent = "💯 Score: " + score;
     if (score < 0) {
       score = 0;
       displayResult("😩 GAME OVER !");
@@ -29,7 +34,7 @@ document.querySelector(".btnReset").addEventListener("click", function () {
   displayResult("⚡Start Guessing.....");
   sn = Math.trunc(Math.random() * 10) + 1;
   score = 10;
-  document.querySelector(".score").textContent = "Score: " + score;
+  document.querySelector(".score").textContent = "💯 Score: " + score;
   document.querySelector(".guess").value = "";
   document.querySelector(".number").textContent = "?";
 });
